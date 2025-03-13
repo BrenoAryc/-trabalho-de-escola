1cbf427041267c5d97aa99765ad04cd74aa81417
Breno <bbrenooliveirasantos@gmail.com>
2025-03-13 08:47:57 -0300

    trabalho do colégio

---
+++ Trabalho diferenças.py
@@ -0,0 +1,4 @@
+Valor1 = int(input("Digite o primeiro valor"))
+Valor2 = int(input("Digite o segundo valor"))
+media = float(Valor1 + Valor2) /2
+print(media)
---
+++ Trabalho.py
@@ -0,0 +1,4 @@
+nome = input("Qual é seu nome?")
+idade = int(input("Quantos anos você tem? "))
+soma = idade + 5
+print(f"Olá,{nome}! Você tem {idade} anos, daqui a 5 anos você terá {soma} anos.")
